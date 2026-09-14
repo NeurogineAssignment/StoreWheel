d09a44f563d17de409909ea5f35934cb158df9a5
omarboshra <osarious2@gmail.com>
2026-09-14 10:28:25 +0300

    Add URL constants

---
+++ app/src/main/java/com/example/storewheel/network/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.storewheel.network.utils
+
+object Constants {
+    const val BASE_URL = "https://dummyjson.com/"
+    const val PRODUCTS_URL = "products?limit=20&skip=0"
+    const val SEARCH_PRODUCTS_URL = "products/search"
+}
