a2ec915a9f47d632b385a68fc5722bda702702ff
omarboshra <osarious2@gmail.com>
2026-09-14 12:03:18 +0300

    Update Repository with getProductById function

--- app/src/main/java/com/example/storewheel/data/repositories/ProductsRepository.kt
+++ app/src/main/java/com/example/storewheel/data/repositories/ProductsRepository.kt
@@ -1,5 +1,6 @@
 package com.example.storewheel.data.repositories
 
+import com.example.storewheel.domain.models.ProductDetailsModel
 import com.example.storewheel.domain.models.ProductModel
 import com.example.storewheel.network.api.ProductsApi
 import javax.inject.Inject
@@ -34,6 +35,23 @@ class ProductsRepository @Inject constructor(private val productsApi: ProductsAp
                 )
             }
             return Result.success(domainList)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun getProductById(id: Int): Result<ProductDetailsModel> {
+        return try {
+            val response = productsApi.getProductById(id)
+            val productDetailsModel = ProductDetailsModel(
+                id = response.id ?: 0,
+                title = response.title ?: "",
+                imageUrl = response.images ?: emptyList(),
+                price = response.price ?: 0.0,
+                rating = response.rating ?: 0f,
+                description = response.description ?: "",
+            )
+            return Result.success(productDetailsModel)
         } catch (e: Exception) {
             Result.failure(e)
         }
